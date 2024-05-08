20f1172b0eb440a5868125d9fc0b06a944bb89e1
ali alahyarlou <alialahyarlou@gmail.com>
2024-05-08 18:45:45 +0330

    Initialize project and install dependency

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    printWidth: 80,
+    singleQuote: true,
+    quoteProps: "consistent",
+    jsxSingleQuote: true,
+    trailingComma: "all",
+  };
+  
